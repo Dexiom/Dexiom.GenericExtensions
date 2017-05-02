fe74a6df7005670c7ed453140b893f70affc5b3e
Jonathan Paré <jpare@dexiom.com>
2017-05-01 22:56:15 -0400

    Initial Commit

---
+++ Dexiom.GenericExtensions/GenericExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Dexiom.GenericExtensions
+{
+    public static class GenericExtensions
+    {
+        public static T With<T>(this T source, Action<T> Method)
+        {
+            Method(source);
+            return source;
+        }
+
+        public static bool In<T>(this T source, params T[] Items)
+        {
+            return Items.Contains(source);
+        }
+
+        public static bool NotIn<T>(this T source, params T[] Items)
+        {
+            return !In(source, Items);
+        }
+
+        public static bool IsDefaultForType<T>(this T source)
+        {
+            return EqualityComparer<T>.Default.Equals(source, default(T));
+        }
+    }
+}
---
+++ Dexiom.GenericExtensionsTests/GenericExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dexiom.GenericExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace Dexiom.GenericExtensions.Tests
+{
+    [TestClass()]
+    public class GenericExtensionsTests
+    {
+        [TestMethod()]
+        public void WithTest()
+        {
+            var temp = new Exception();
+            temp.With(n => n.Source = "toto");
+            Assert.IsTrue(temp.Source == "toto");
+        }
+
+        [TestMethod()]
+        public void InTest()
+        {
+            const string temp = "toto";
+            Assert.IsTrue(temp.In("toto", "titi", "tata"));
+            Assert.IsFalse(temp.In("titi", "tata"));
+        }
+
+        [TestMethod()]
+        public void NotInTest()
+        {
+            const string temp = "toto";
+            Assert.IsFalse(temp.NotIn("toto", "titi", "tata"));
+            Assert.IsTrue(temp.NotIn("titi", "tata"));
+        }
+
+        [TestMethod()]
+        public void IsDefaultForTypeTest()
+        {
+            int intValue1 = 0;
+            int intValue2 = 1;
+            Assert.IsTrue(intValue1.IsDefaultForType());
+            Assert.IsFalse(intValue2.IsDefaultForType());
+        }
+    }
+}
